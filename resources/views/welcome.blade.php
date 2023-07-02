0f4e1fb0b5f9a425c756d200193b3dc0c3aee33f
Hoan Tran <trminhhoan@gmail.com>
2023-07-02 12:34:29 +0700

    update logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,22 +14,22 @@
         <section id="header">
             <div class="logo">
                 <a class="img-logo" href="#">
-                    <img src="{{ asset('images/logo.png') }}" alt="Logo" class="logo">
+                    <img src="{{ asset('images/logo3.png') }}" alt="Logo" class="logo">
                 </a>
             </div>
 
             <div class="user">
                 <a class="img-user" href="#">
-                    <img src="{{ asset('images/dog.jpg') }}" alt="Logo" class="logo">
+                    <img src="{{ asset('images/avatar2.png') }}" alt="Logo" class="logo">
                 </a>
             </div>
 
             <div class="name-user">
                 <a href="#" class="name">
-                    Tran Minh Hoan
+                    Handicrafted by
                 </a>
                 <a href="">
-                    user
+                    Jim HLS
                 </a>
             </div>
         </section>
@@ -42,40 +42,46 @@
         </section>
 
         <section id="content">
-            <div class="content-text">
-                <p class="auto-text">A child asked his father, "How were people born?" So his father said, "Adam and Eve
-                    made babies, then
-                    their babies became adults and made babies, and so on."
-                    The child then went to his mother, asked her the same question and she told him, "We were monkeys
-                    then we evolved to become like we are now."
-                    The child ran back to his father and said, "You lied to me!" His father replied, "No, your mom was
-                    talking about her side of the family."</p>
-            </div>
+            <div class="content-firt">
+                <div class="content-text">
+                    <p class="auto-text">A child asked his father, "How were people born?" So his father said, "Adam and
+                        Eve
+                        made babies, then
+                        their babies became adults and made babies, and so on."
+                        The child then went to his mother, asked her the same question and she told him, "We were
+                        monkeys
+                        then we evolved to become like we are now."
+                        The child ran back to his father and said, "You lied to me!" His father replied, "No, your mom
+                        was
+                        talking about her side of the family."</p>
+                </div>
 
-            <div class="horizontal-line"></div>
+                <div class="horizontal-line"></div>
 
-            <div class="content-button">
-                <button class="button-like">
-                    This is funny!
-                </button>
+                <div class="content-button">
+                    <button class="button-like">
+                        This is funny!
+                    </button>
 
-                <button class="button-dislike">
-                    This is not funny!
-                </button>
+                    <button class="button-dislike">
+                        This is not funny!
+                    </button>
+                </div>
             </div>
         </section>
 
         <section id="footer">
-            <div class="footer-content">
-                <p>
-                    This app will display a single joke for the user to read. After reading the joke, the user will like
-                    or dislike the joke. The app will record the vote in database and then show another joke for the
-                    user to read. When there is no more jokes to show, the app will display a "That's all the jokes for
-                    today! Come back another day!" message.
-                    There is no need to display the result of the votes. User should not see the same joke twice. User
-                    do not need to register or login to view the joke or vote for the joke.
-                </p>
-                <div class="content-secsion">HoanTran 2023 TMH</div>
+            <div class="footer-main">
+                <div class="footer-content">
+                    <p>
+                        This website is created as part of Hisolutions program. The materials contained
+                        on this website are provided for general information only and do not constilute
+                        any from of advice. HLS assumes no responsibility for the accuracy of any particular
+                        statement and accecpts no liability for any loss or damage which may arise from rellance
+                        on the information contained on this site.
+                    </p>
+                    <div class="content-secsion">Copyright 2021 HLS</div>
+                </div>
             </div>
         </section>
     </section>
